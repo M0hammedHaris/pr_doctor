c4a393782ac7575f58f5e3f9d55f6b8427d6c8de
Haris2032 <mohammedharis.k.01@gmail.com>
2020-01-30 21:41:51 +0530

    New home design

--- lib/screens/main_screen/home_screen.dart
+++ lib/screens/main_screen/home_screen.dart
@@ -13,74 +13,101 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   String name;
+  TextEditingController _changedName = TextEditingController();
 
-  getNameFromDb() async{
+  getNameFromDb() async {
     final database = Provider.of<AppDatabase>(context, listen: false);
     final data = await database.getAllData();
 
-  setState(() {
-    name = data.userName;
-  });
+    setState(() {
+      name = data.userName;
+    });
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getNameFromDb();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          flexibleSpace: Container(
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                      colors: <Color>[
-                Colors.cyan,
-                Colors.cyan[200],
-              ])))),
-      body: Center(
-          child: Padding(
-        padding: EdgeInsets.all(5.0),
-        child: ListView(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+                top: 25.0, left: 8.0, bottom: 10.0, right: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
-                Text("Welcome",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30.0,
-                  fontFamily: "Montserrat",
-                  letterSpacing: 0.5,
-                )),
+
+               IconButton(
+                  icon: Icon(Icons.menu),
+                  color: Colors.cyan,
+                  visualDensity: VisualDensity(horizontal: 1.5,vertical: 1.5),
+                  onPressed: () {
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.search),
+                  color: Colors.cyan,
+                  visualDensity: VisualDensity(horizontal: 1.5,vertical: 1.5),
+                  onPressed: () {},
+                )
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(name,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 25.0,
-                  fontFamily: "Montserrat",
-                )),
-              ],
-            )
-          ],
-        ),
-      )),
+          ),
+          Padding(padding: EdgeInsets.only(
+                top: 10.0, left: 8.0 ,right: 8.0),child:Container(
+            padding: EdgeInsets.only(
+                top: 10.0, left: 8.0, right: 8.0),
+            height: MediaQuery.of(context).size.height - 100,
+            child: Padding(
+              padding: EdgeInsets.all(5.0),
+              child: ListView(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text("Welcome",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 30.0,
+                            fontFamily: "Montserrat",
+                            letterSpacing: 0.5,
+                          )),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(name,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 25.0,
+                            fontFamily: "Montserrat",
+                          )),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ))
+        ],
+      ),
       drawer: _drawer(),
       floatingActionButton: FloatingActionButton(
           child: Icon(Icons.chat_bubble_outline),
           onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => ChatBot(name)));
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => ChatBot(name)));
           }),
     );
   }
+
   Widget _drawer() {
     return Drawer(
       elevation: 20,
@@ -93,48 +120,114 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               child: null),
           ListTile(
+              leading: Icon(Icons.person_pin, color: Colors.black),
+              title: Text('Change Name'),
+              onTap: () {
+                Navigator.pop(context);
+                _nameChangeAlert();
+              }),
+          Divider(),
+          ListTile(
               leading: Icon(Icons.exit_to_app, color: Colors.black),
               title: Text('Log Out'),
               onTap: () {
                 Navigator.pop(context);
-               _logOutAlert();
+                _logOutAlert();
               }),
           Divider(),
         ],
       ),
     );
   }
-  Future<void> _logOutAlert() async {
-  return showDialog<void>(
-    context: context,
-    barrierDismissible: false, // user must tap button!
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text('Log Out',textScaleFactor: 1.2,),
-        content: Text('Are you sure ?',textScaleFactor: 1,),
-        actions: <Widget>[
-          FlatButton(
-            child: Text('Yes'),
-            onPressed: () {
-              _logout();
-              Navigator.of(context).pop();
-            },
-          ),
-          FlatButton(
-            child: Text('Regret'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      );
-    },
-  );
-}
 
-void _logout() async{
-  final database = Provider.of<AppDatabase>(context, listen: false);
-    final data = await database.resetDb();
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage(),));
-}
+  Future<void> _logOutAlert() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Log Out',
+            textScaleFactor: 1.2,
+          ),
+          content: Text(
+            'Are you sure ?',
+            textScaleFactor: 1,
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () {
+                _logout();
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Regret'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _nameChangeAlert() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(
+            'Enter new name',
+            textScaleFactor: 1.2,
+          ),
+          content: TextField(
+            controller: _changedName,
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "New user Name",
+                hintStyle: TextStyle(color: Colors.grey[400])),
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Yes'),
+              onPressed: () {
+                _updateUserName();
+                Navigator.of(context).pop();
+              },
+            ),
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _logout() async {
+    final database = Provider.of<AppDatabase>(context, listen: false);
+    await database.resetDb();
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LoginPage(),
+        ));
+  }
+
+  void _updateUserName() async {
+    final database = Provider.of<AppDatabase>(context, listen: false);
+    final data = await database.getAllData();
+    database.updateTask(
+        data.copyWith(userName: _changedName.text, id: 1, validate: true));
+    setState(() {
+      getNameFromDb();
+    });
+  }
 }
